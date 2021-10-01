22b9ae2bd53b81cb251ba5daf7745f9934d4b6f4
unknown <matthewajustice@gmail.com>
2021-10-01 10:59:16 -0400

    Added minor styling to the main screen, title and addbutton

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import colors from './colors';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
+      <View style={{ flexDirection: 'row' }}>
+        <View style={styles.divider} />
+        <Text style={styles.title}>
+          Tuex <Text style={{ color: colors.blue }}>Duex</Text>
+        </Text>
+        <View style={styles.divider} />
+      </View>
+
+      <View style={{ marginVertical: 48 }}>
+        <TouchableOpacity style={styles.addButton}>
+          <AntDesign name='plus' size={16} color={colors.blue} />
+        </TouchableOpacity>
+
+        <Text style={styles.addText}>Add List</Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +30,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'whitesmoke',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  divider: {
+    backgroundColor: colors.lightBlue,
+    height: 2,
+    flex: 1,
+    alignSelf: 'center',
+  },
+
+  title: {
+    fontSize: 38,
+    fontWeight: 'bold',
+    color: colors.black,
+    paddingHorizontal: 64,
+  },
+
+  addButton: {
+    borderWidth: 2,
+    borderColor: colors.lightBlue,
+    borderRadius: 4,
+    padding: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  addText: {
+    color: colors.blue,
+    fontWeight: 'bold',
+    fontSize: 14,
+    marginTop: 8,
   },
 });
